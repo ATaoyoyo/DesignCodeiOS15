7d6c59f1a467a572b73d4424062d34567b5e2d99
ATaoyoyo <1029187998@qq.com>
2022-05-02 18:54:23 +0800

    Initial Commit

---
+++ DesignCodeiOS15/DesignCodeiOS15App.swift
@@ -0,0 +1,17 @@
+//
+//  DesignCodeiOS15App.swift
+//  DesignCodeiOS15
+//
+//  Created by Yoyo ATao on 2022/5/2.
+//
+
+import SwiftUI
+
+@main
+struct DesignCodeiOS15App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
